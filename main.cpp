ffc20eba5c260afef2f9fe5da8f83d25eb2e7a52
Katya <guelder.rose8@gmail.com>
2026-04-24 22:00:41 +0000

    First state

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    a = 1;
+    int b;
+    b = 2;
+
+    int c;
+    c = a + b;    
+    std::cout << a << " + " << b << " = " << c << '\n';
+    
+    int d;
+    d = a - b;    
+    std::cout << a << " - " << b << " = " << d << '\n';
+    
+    int e;
+    e = a * b;    
+    std::cout << a << " * " << b << " = " << e << '\n';
+}
