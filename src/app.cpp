70fc4a22babe54eae264f84d996a6474df5a545e
Katya <guelder.rose8@gmail.com>
2026-04-29 00:01:23 +0000

    Removed local lib, removed std things, changed reading of command line arguments

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,9 @@
 #include "app.h"
 #include "libmath.h"
 
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
 
 namespace
 {
@@ -17,9 +19,27 @@ struct Task
 
 void parse(int argc, char** argv, Task& task)
 {
-    task.value1 = std::atoi(argv[1]);
-    task.operation = *(argv[2]);
-    task.value2 = std::atoi(argv[3]);
+    int opt;
+
+    while ((opt = getopt(argc, argv, "asmdpf") != -1) {
+switch (opt) {
+case 'a': task.operation = '+'; break;
+case 's': task.operation = '-'; break;
+            case 'm': task.operation = '*'; break;
+            case 'd': task.operation = '/'; break;
+            case 'p': task.operation = '^'; break;
+            case 'f': task.operation = '!'; break;
+            default: task.operation = '?'; return;
+}
+}
+	
+    if(task.operation == '!'){
+    task.value1 = atoi(argv[optind]);
+    }
+    else {
+    task.value1 = atoi(argv[optind]);
+    task.value2 = atoi(argv[optint + 1]);
+    }    
 }
 
 void calculate(Task& task)
@@ -28,19 +48,19 @@ void calculate(Task& task)
     switch(task.operation)
     {
         case '+':
-                task.result = libmath::addition(task.value1, task.value2);
+                task.status = libmath::addition(task.value1, task.value2, task.result);
                 break;
         case '-':
-                task.result = libmath::subtraction(task.value1, task.value2);
+                task.status = libmath::subtraction(task.value1, task.value2, task.result);
                 break;
         case '*':
-                task.result = libmath::multiplication(task.value1, task.value2);
+                task.status = libmath::multiplication(task.value1, task.value2, task.result);
                 break;
         case '/':
                 task.status = libmath::division(task.value1, task.value2, task.result);
                 break;
         case '^':
-                task.result = libmath::power(task.value1, task.value2);
+                task.status = libmath::power(task.value1, task.value2, task.result);
                 break;
         default:
                 task.status = 1;
@@ -52,19 +72,36 @@ void output(Task task)
 {
     if(task.status == 0)
     {
-        std::cout << task.value1 << ' ' << task.operation << ' ' << task.value2 << " = " << task.result << '\n';
+	if(task.operation == '!')
+	{
+		printf("%d! = %d\n", task.value1, task.result);
+	}else {
+             printf("%d %c %d = %d\n", task.value1, task.operation, task.value2, task.result);
+	}
     }
     else if (task.status == -1)
     {
-        std::cout << "Error! Division by zero!\n";
+        printf("Error! Division by zero!\n");
     }
     else if (task.status == 1)
     {
-        std::cout << "Error! Unknown operation!\n";
+        printf("Error! Unknown operation!\n");
+    }
+    else if (task.status == 2)
+    {
+        printf("Error! Raising to a negative power!\n");
+    }
+    else if (task.status == 3)
+    {
+        printf("Error! Factorial of a negative number!\n");
+    }
+    else if (task.status == 4)
+    {
+        printf("Error! Type overflow!\n");
     }
     else
     {
-        std::cout << "Unknown error\n";
+        printf("Unknown error\n");
     }
 }
 
