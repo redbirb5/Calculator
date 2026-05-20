1ab13e3d8c07299d7af9e4311e6b2fa648c5b7e3
Katya <guelder.rose8@gmail.com>
2026-05-20 11:21:55 +0000

    Refactor: make classes instead of functions

--- src/app.h
+++ src/app.h
@@ -1,6 +1,42 @@
 #pragma once
 
+#include <optional>
+#include <string>
+
 namespace app
 {
-void run(int argc, char** argv);
+    //void run(int argc, char** argv);
+
+    struct Request{
+        std::string operation;
+        int value1;
+        std::optional<int> value2;
+    };
+
+    class JsonParser{
+        public:
+        Request parse(int argc, char** argv) const;
+    };
+
+    class Calculator{
+        public:
+        int calculate(const Request& request) const;
+    };
+
+    class Printer{
+        public:
+        void print(const Request& request, int result) const;
+    };
+
+    class CalculatorApp{
+        public:
+        int run(int argc, char** argv);
+
+        private:
+        JsonParser parser_;
+        Calculator calculator_;
+        Printer printer_;
+    };
+
+    
 }
