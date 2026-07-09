cbb37cf6be32e13588f32566a85f94965a609ea2
Katya <guelder.rose8@gmail.com>
2026-07-09 21:36:24 +0000

    feat: read database config from json

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace app
+{
+std::string readConnectionString(const std::string& config_path);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
+#include "config.h"
+
 #include "app.h"
 
+#include <exception>
+#include <iostream>
 #include <string>
 
 int main(int argc, char** argv)
 {
-    const std::string connection_string =
-        "host=localhost port=5432 dbname=calculator_db user=calculator_app password=1212";
-    app::CalculatorApp calc(connection_string);
-    return calc.run(argc, argv);
+    try
+    {
+        const std::string connection_string =
+            app::readConnectionString("config.json");
+        app::CalculatorApp calc(connection_string);
+        return calc.run(argc, argv);
+    }
+    catch (const std::exception& err)
+    {
+        std::cerr << "Error! " << err.what() << "\n";
+        return 1;
+    }
 }
